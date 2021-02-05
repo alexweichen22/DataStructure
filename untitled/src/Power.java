a9c211acfeaf77f74c26ce551e10127aca3b2c9f
alexweichen22 <alexweichen22@gmail.com>
2021-02-05 08:56:28 -0500

    update

---
+++ untitled/src/Power.java
@@ -0,0 +1,12 @@
+public class Power {
+
+    public static void main(String args){
+        System.out.println(powerBase(5.0,2);
+
+    }
+
+    private static double powerBase(double base, int n){
+        if(n==1)return base;
+        return powerBase(base,n-1);
+    }
+}
---
+++ untitled1/src/com/ipd22/Main.java
@@ -0,0 +1,19 @@
+package com.ipd22;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(powerBase(5.0,2);
+    }
+
+    private static double powerBase(double base, int n){
+        if(n==1)return base;
+        return powerBase(base,n-1);
+    }
+
+    private static int countHi(String input){
+        if(str)
+
+    }
+}
